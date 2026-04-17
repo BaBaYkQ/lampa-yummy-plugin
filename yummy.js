d76cc3dbe193d856233980e8c54b2cad0ecfa12c
BaBaYkQ <babayk0.q1@gmail.com>
2026-04-17 12:38:18 +0300

    Rename test_plugin to yummy_plugin and update messages

--- yummy.js
+++ yummy.js
@@ -1,11 +1,25 @@
 (function () {
-    if (window.test_plugin_loaded) return;
-    window.test_plugin_loaded = true;
+    if (window.yummy_plugin_loaded) return;
+    window.yummy_plugin_loaded = true;
 
     function start() {
-        console.log('TEST PLUGIN START');
+        console.log('Yummy menu старт');
 
-        Lampa.Noty.show('TEST plugin працює!');
+        Lampa.Menu.add({
+            title: 'YummyAnime',
+            icon: '🎬',
+            component: 'yummy_main'
+        });
+
+        function YummyMain() {
+            this.create = function () {
+                Lampa.Noty.show('YummyAnime меню працює!');
+            };
+        }
+
+        Lampa.Component.add('yummy_main', YummyMain);
+
+        Lampa.Noty.show('YummyAnime додано в меню');
     }
 
     if (window.appready) start();
