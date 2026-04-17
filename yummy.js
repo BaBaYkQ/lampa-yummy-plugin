50fe837574cca49ace80d4c4867d6657841830d0
BaBaYkQ <babayk0.q1@gmail.com>
2026-04-17 12:35:34 +0300

    Replace Yummy plugin initialization with Test plugin

--- yummy.js
+++ yummy.js
@@ -1,107 +1,11 @@
 (function () {
-    if (window.yummy_full_loaded) return;
-    window.yummy_full_loaded = true;
-
-    const API_TOKEN = 'k8deq_gljhple1r3q-ia-b7u-7ee3lbh';
-    const API_BASE = 'https://api.yani.tv';
-
-    function api(url, success, error) {
-        Lampa.Network.get(url, success, error, {
-            headers: {
-                'X-Application': API_TOKEN
-            }
-        });
-    }
-
-    function YummyMain() {
-        this.create = function () {
-            this.activity.loader(true);
-
-            api(`${API_BASE}/anime?limit=40`, (resp) => {
-                try {
-                    const json = JSON.parse(resp);
-                    const items = json.response || [];
-
-                    const cards = items.map(item => ({
-                        title: item.title,
-                        poster: item.poster_url,
-                        description: item.description,
-                        id: item.id,
-                        component: 'yummy_card'
-                    }));
-
-                    this.activity.loader(false);
-                    this.activity.push(cards);
-
-                } catch (e) {
-                    console.error(e);
-                    this.activity.loader(false);
-                }
-            }, () => this.activity.loader(false));
-        };
-    }
-
-    function YummyCard(object) {
-        this.create = function () {
-            this.activity.loader(true);
-
-            api(`${API_BASE}/anime/${object.id}?include_videos=true`, (resp) => {
-                try {
-                    const json = JSON.parse(resp);
-                    const anime = json.response;
-                    const videos = anime.videos || [];
-
-                    const episodes = videos.map(ep => ({
-                        title: 'Серія ' + ep.episode,
-                        file: ep.stream_url
-                    }));
-
-                    this.activity.loader(false);
-
-                    Lampa.Player.play({
-                        title: anime.title,
-                        playlist: episodes
-                    });
-
-                } catch (e) {
-                    console.error(e);
-                    this.activity.loader(false);
-                }
-            }, () => this.activity.loader(false));
-        };
-    }
-
-    function addMenuButton() {
-        Lampa.Template.add('yummy_menu_btn', `
-            <li class="menu__item selector" data-action="yummyanime">
-                <div class="menu__ico">🎬</div>
-                <div class="menu__text">YummyAnime</div>
-            </li>
-        `);
-
-        Lampa.Listener.follow('menu', function (e) {
-            if (e.type === 'ready') {
-                $('.menu .menu__list').append(
-                    Lampa.Template.get('yummy_menu_btn')
-                );
-            }
-
-            if (e.type === 'select' && e.action === 'yummyanime') {
-                Lampa.Activity.push({
-                    title: 'YummyAnime',
-                    component: 'yummy_main'
-                });
-            }
-        });
-    }
+    if (window.test_plugin_loaded) return;
+    window.test_plugin_loaded = true;
 
     function start() {
-        Lampa.Component.add('yummy_main', YummyMain);
-        Lampa.Component.add('yummy_card', YummyCard);
+        console.log('TEST PLUGIN START');
 
-        addMenuButton();
-
-        console.log('YummyAnime FULL plugin loaded');
+        Lampa.Noty.show('TEST plugin працює!');
     }
 
     if (window.appready) start();
