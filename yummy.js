40e8d2e8f019a0127edd77d8c45f3c0a1ba940a7
BaBaYkQ <babayk0.q1@gmail.com>
2026-04-17 12:48:29 +0300

    Refactor YummyAnime plugin and improve loading logic

--- yummy.js
+++ yummy.js
@@ -3,26 +3,106 @@
     window.yummy_plugin_loaded = true;
 
     function start() {
+
+        // ===== ГОЛОВНА СТОРІНКА =====
         Lampa.Component.add('yummy_main', {
             create: function () {
-                Lampa.Noty.show('YummyAnime відкрито');
+                const activity = this.activity;
+                activity.loader(true);
+
+                Lampa.Network.get(
+                    'https://api.yani.tv/anime?limit=30',
+                    (resp) => {
+                        try {
+                            const json = JSON.parse(resp);
+                            const items = json.response || [];
+
+                            activity.loader(false);
+
+                            activity.push(items.map(i => ({
+                                title: i.title,
+                                poster: i.poster_url,
+                                id: i.id,
+                                component: 'yummy_card'
+                            })));
+
+                        } catch (e) {
+                            console.error(e);
+                            activity.loader(false);
+                        }
+                    },
+                    () => activity.loader(false)
+                );
             }
         });
 
-        if (!Lampa.Manifest.plugins) Lampa.Manifest.plugins = [];
+        // ===== КАРТКА АІМЕ =====
+        Lampa.Component.add('yummy_card', {
+            create: function (object) {
+                const activity = this.activity;
+                activity.loader(true);
 
-        Lampa.Manifest.plugins.push({
-            type: 'video',
-            name: 'YummyAnime',
-            component: 'yummy_main'
+                Lampa.Network.get(
+                    `https://api.yani.tv/anime/${object.id}?include_videos=true`,
+                    (resp) => {
+                        try {
+                            const json = JSON.parse(resp);
+                            const anime = json.response;
+                            const videos = anime.videos || [];
+
+                            const playlist = videos.map(v => ({
+                                title: 'Серія ' + v.episode,
+                                file: v.stream_url
+                            }));
+
+                            activity.loader(false);
+
+                            Lampa.Player.play({
+                                title: anime.title,
+                                playlist: playlist
+                            });
+
+                        } catch (e) {
+                            console.error(e);
+                            activity.loader(false);
+                        }
+                    },
+                    () => activity.loader(false)
+                );
+            }
         });
 
-        Lampa.Noty.show('YummyAnime plugin додано');
+        // ===== МЕНЮ (100% РОБОЧЕ В 1.12.4) =====
+        Lampa.Listener.follow('menu', function (e) {
+
+            if (e.type === 'ready') {
+
+                const html = `
+                    <li class="menu__item selector" data-action="yummy_open">
+                        <div class="menu__ico">🎬</div>
+                        <div class="menu__text">YummyAnime</div>
+                    </li>
+                `;
+
+                $('.menu__list').append(html);
+            }
+
+            if (e.type === 'select' && e.action === 'yummy_open') {
+
+                Lampa.Activity.push({
+                    title: 'YummyAnime',
+                    component: 'yummy_main'
+                });
+            }
+        });
+
+        Lampa.Noty.show('YummyAnime встановлено');
+        console.log('Yummy plugin loaded');
     }
 
     if (window.appready) start();
     else {
-        Lampa.Listener.follow('app', function (e) {
+        Lampa.Listener.follow('app', e => {
             if (e.type === 'ready') start();
         });
     }
