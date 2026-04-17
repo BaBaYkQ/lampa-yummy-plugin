e69e32da212663dafe7a9edf66a91a8bcff1dd68
BaBaYkQ <babayk0.q1@gmail.com>
2026-04-17 12:56:02 +0300

    Refactor YummyAnime plugin to Yummy ULTRA

--- yummy.js
+++ yummy.js
@@ -1,109 +1,210 @@
 (function () {
-    if (window.yummy_plugin_loaded) return;
-    window.yummy_plugin_loaded = true;
+    if (window.yummy_ultra_loaded) return;
+    window.yummy_ultra_loaded = true;
 
+    const API = 'https://api.yani.tv';
+    const TOKEN = 'k8deq_gljhple1r3q-ia-b7u-7ee3lbh';
+
+    function api(url, ok, fail) {
+        Lampa.Network.get(url, ok, fail, {
+            headers: { 'X-Application': TOKEN }
+        });
+    }
+
+    // =========================
+    // SOURCE (як Kodik)
+    // =========================
+    Lampa.Api.sources.yummy = {
+        title: 'YummyAnime ULTRA',
+
+        search: function (object, params) {
+            const q = object.title || '';
+
+            api(`${API}/anime/search?query=${encodeURIComponent(q)}`, (r) => {
+                try {
+                    const j = JSON.parse(r);
+                    const items = j.response || [];
+
+                    if (!items.length) return params.error();
+
+                    params.success(items.map(i => ({
+                        title: i.title,
+                        id: i.id,
+                        poster: i.poster_url
+                    })));
+
+                } catch (e) {
+                    console.error(e);
+                    params.error();
+                }
+            }, params.error);
+        },
+
+        tv: function (item, params) {
+            api(`${API}/anime/${item.id}?include_videos=true`, (r) => {
+                try {
+                    const j = JSON.parse(r);
+                    const anime = j.response;
+                    const vids = anime.videos || [];
+
+                    const episodes = vids.map(v => {
+                        let file = v.stream_url;
+
+                        if (v.qualities?.length) {
+                            file = v.qualities[0].url;
+                        }
+
+                        return {
+                            title: 'Серія ' + v.episode,
+                            file: file,
+                            episode: v.episode
+                        };
+                    });
+
+                    params.success({
+                        title: anime.title,
+                        episodes: episodes
+                    });
+
+                } catch (e) {
+                    console.error(e);
+                    params.error();
+                }
+            }, params.error);
+        }
+    };
+
+    Lampa.Api.sources.tmdb.push({
+        title: 'YummyAnime ULTRA',
+        source: 'yummy'
+    });
+
+    // =========================
+    // UI
+    // =========================
     function start() {
 
-        // ===== ГОЛОВНА СТОРІНКА =====
+        // кеш (швидкість)
+        const cache = {};
+
+        // головна
         Lampa.Component.add('yummy_main', {
             create: function () {
-                const activity = this.activity;
-                activity.loader(true);
+                const a = this.activity;
 
-                Lampa.Network.get(
-                    'https://api.yani.tv/anime?limit=30',
-                    (resp) => {
-                        try {
-                            const json = JSON.parse(resp);
-                            const items = json.response || [];
+                a.loader(true);
 
-                            activity.loader(false);
+                if (cache.list) {
+                    a.loader(false);
+                    a.push(cache.list);
+                    return;
+                }
 
-                            activity.push(items.map(i => ({
-                                title: i.title,
-                                poster: i.poster_url,
-                                id: i.id,
-                                component: 'yummy_card'
-                            })));
+                api(`${API}/anime?limit=40`, (r) => {
+                    try {
+                        const j = JSON.parse(r);
+                        const items = j.response || [];
 
-                        } catch (e) {
-                            console.error(e);
-                            activity.loader(false);
-                        }
-                    },
-                    () => activity.loader(false)
-                );
+                        cache.list = items;
+
+                        a.loader(false);
+
+                        a.push(items.map(i => ({
+                            title: i.title,
+                            poster: i.poster_url,
+                            id: i.id,
+                            component: 'yummy_card'
+                        })));
+
+                    } catch (e) {
+                        console.error(e);
+                        a.loader(false);
+                    }
+                }, () => a.loader(false));
             }
         });
 
-        // ===== КАРТКА АІМЕ =====
+        // картка (серії вручну, не автоплеєр)
         Lampa.Component.add('yummy_card', {
             create: function (object) {
-                const activity = this.activity;
-                activity.loader(true);
+                const a = this.activity;
+                a.loader(true);
 
-                Lampa.Network.get(
-                    `https://api.yani.tv/anime/${object.id}?include_videos=true`,
-                    (resp) => {
-                        try {
-                            const json = JSON.parse(resp);
-                            const anime = json.response;
-                            const videos = anime.videos || [];
+                api(`${API}/anime/${object.id}?include_videos=true`, (r) => {
+                    try {
+                        const j = JSON.parse(r);
+                        const anime = j.response;
+                        const vids = anime.videos || [];
 
-                            const playlist = videos.map(v => ({
-                                title: 'Серія ' + v.episode,
-                                file: v.stream_url
-                            }));
+                        const last = Lampa.Storage.get('yummy_last_ep_' + object.id);
 
-                            activity.loader(false);
+                        const episodes = vids.map(v => {
+
+                            let file = v.stream_url;
+
+                            if (v.qualities?.length) {
+                                file = v.qualities[0].url;
+                            }
+
+                            return {
+                                title: (v.episode == last ? '▶ ' : '') + 'Серія ' + v.episode,
+                                file: file,
+                                episode: v.episode
+                            };
+                        });
+
+                        a.loader(false);
+
+                        // список серій
+                        a.push(episodes);
+
+                        // клік по серії
+                        a.onSelect = function (item) {
+
+                            Lampa.Storage.set('yummy_last_ep_' + object.id, item.episode);
 
                             Lampa.Player.play({
                                 title: anime.title,
-                                playlist: playlist
+                                playlist: episodes,
+                                start: item.episode
                             });
+                        };
 
-                        } catch (e) {
-                            console.error(e);
-                            activity.loader(false);
-                        }
-                    },
-                    () => activity.loader(false)
-                );
+                    } catch (e) {
+                        console.error(e);
+                        a.loader(false);
+                    }
+                }, () => a.loader(false));
             }
         });
 
-        // ===== МЕНЮ (100% РОБОЧЕ В 1.12.4) =====
+        // меню
         Lampa.Listener.follow('menu', function (e) {
-
             if (e.type === 'ready') {
 
-                const html = `
+                $('.menu__list').append(`
                     <li class="menu__item selector" data-action="yummy_open">
-                        <div class="menu__ico">🎬</div>
-                        <div class="menu__text">YummyAnime</div>
+                        <div class="menu__ico">🍥</div>
+                        <div class="menu__text">Yummy ULTRA</div>
                     </li>
-                `;
-
-                $('.menu__list').append(html);
+                `);
             }
 
             if (e.type === 'select' && e.action === 'yummy_open') {
-
                 Lampa.Activity.push({
-                    title: 'YummyAnime',
+                    title: 'Yummy ULTRA',
                     component: 'yummy_main'
                 });
             }
         });
 
-        Lampa.Noty.show('YummyAnime встановлено');
-        console.log('Yummy plugin loaded');
+        Lampa.Noty.show('Yummy ULTRA активовано');
+        console.log('Yummy ULTRA loaded');
     }
 
     if (window.appready) start();
-    else {
-        Lampa.Listener.follow('app', e => {
-            if (e.type === 'ready') start();
-        });
-    }
+    else Lampa.Listener.follow('app', e => {
+        if (e.type === 'ready') start();
+    });
+
 })();
